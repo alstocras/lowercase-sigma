84172fea54749b6ca5161dbb97d02a4955ef74ad
Alstocras <alstoperiment@gmail.com>
2025-11-30 10:17:43 +0000

    Javadoc?

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.alstocras.sigma","l":"InterstellarObject"},{"p":"com.alstocras.sigma","l":"Main"},{"p":"com.alstocras.sigma","l":"Remnant"},{"p":"com.alstocras.sigma","l":"Star"}];updateSearchResults();
