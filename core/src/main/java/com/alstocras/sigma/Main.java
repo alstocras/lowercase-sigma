c2f5249ffc12cd5ee08c9c941344254cfcfe62a6
Alstocras <alstoperiment@gmail.com>
2026-03-21 18:28:01 +0000

    starter hexagon implementation WIP

--- core/src/main/java/com/alstocras/sigma/Main.java
+++ core/src/main/java/com/alstocras/sigma/Main.java
@@ -2,28 +2,41 @@ package com.alstocras.sigma;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.*;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.*;
+import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.awt.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private ShapeRenderer shape;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        shape = new ShapeRenderer();
+    }
+
+    private Vector2 makeHexagon(Vector2 centre, double radius, int iterator, Color colour){
+        double angle = ((Math.PI / 3) * iterator) - (Math.PI / 6);
+        float newCentreX = (float)(centre.x + radius * Math.cos(angle));
+        float newCentreY = (float)(centre.y + radius * Math.sin(angle));
+        return new Vector2(newCentreX, newCentreY);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        shape.begin(ShapeType.Line);
+
     }
 
     @Override
