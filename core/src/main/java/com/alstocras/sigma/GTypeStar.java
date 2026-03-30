f54d9c94648edbea987f293877cf0f318e5b113b
Alstocras <alstoperiment@gmail.com>
2026-03-30 01:40:01 +0000

    new sprites and random colours

--- core/src/main/java/com/alstocras/sigma/GTypeStar.java
+++ core/src/main/java/com/alstocras/sigma/GTypeStar.java
@@ -10,9 +10,10 @@ import java.util.*;
 public class GTypeStar extends MainSequenceStar{
     final double MAX_AGE_SECONDS = 3.154e18;
     final boolean CAN_BLACK_HOLE = false;
-    final Color STAR_COLOUR = Color.valueOf("fffdd4ff");
+    Color starColour;
     Sprite sprite;
     SpriteBatch batch;
+    Random random;
 
     public GTypeStar(double mass, double radius, AxialCoordinate coord, Sprite sprite, SpriteBatch batch) {
         super(coord);
@@ -21,6 +22,11 @@ public class GTypeStar extends MainSequenceStar{
         this.coordinate = coord;
         this.sprite = sprite;
         this.batch = batch;
+        random = new Random();
+        float r = 0.9f + random.nextFloat() * 0.1f;
+        float g = 0.96f + random.nextFloat() * 0.04f;
+        float b = 0.78f + random.nextFloat() * 0.1f;
+        starColour = new Color(r, g, b, 1f);
     }
 
     @Override
@@ -29,7 +35,7 @@ public class GTypeStar extends MainSequenceStar{
         sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
         batch.setProjectionMatrix(Main.camera.combined);
         batch.begin();
-        sprite.setColor(STAR_COLOUR);
+        sprite.setColor(starColour);
         sprite.setPosition((HexGridGenerator.axialToCartesian(this.coordinate, hexRadius)).x - sprite.getOriginX(), (HexGridGenerator.axialToCartesian(this.coordinate, hexRadius)).y - sprite.getOriginY());
         sprite.draw(batch);
         batch.end();
